12d4545a07ee14ef33dc4b645345ca88c954d08d
Jean <jeanpolo15@gmail.com>
2024-02-15 09:55:40 -0500

    Se agrego Pantalla inicial

--- Hiupy/src/pages/Home.jsx
+++ Hiupy/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { NavBar } from "../components";
+import { Boton } from "../components/Boton/Boton.jsx";
 
 
 
@@ -6,7 +7,13 @@ export const Home = () => {
     return (
         <>
             <NavBar/>
-            <h1 className="text-center">Bienvenidos a Hiupy!</h1>
+            <div className="bg-[url('./images/fondoalimentos-1.png')] bg-cover 
+            bg-center min-h-screen flex flex-col items-center justify-center">
+                <img src="../images/logo.png" alt="Logo"/>
+                <h1 className="text-center font-semibold text-5xl text-white">Nuestro Alimento</h1>
+                <p className="text-center text-base text-white">La forma de conectarnos con todo lo que nos rodea</p>
+                <Boton texto="Comenzar" />
+            </div>
         </>
     )
 };
