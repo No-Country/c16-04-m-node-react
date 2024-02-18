18353050a481eaab0df085db0546ffe54c7ff316
Jean <jeanpolo15@gmail.com>
2024-02-18 00:53:55 -0500

    modificacion de props

--- Hiupy/src/components/Boton/Boton.jsx
+++ Hiupy/src/components/Boton/Boton.jsx
@@ -3,19 +3,22 @@ import { NavLink } from "react-router-dom";
 
 
 export const Boton = (props) => {
+
+    const {texto, fondo, ruta, etiqueta} = props;  //nota: deconstruccion de props
+
     console.log(props);
     return (
         <div className="flex flex-col items-center">
-            <p className="text-sm">{props.texto}</p>
+            <p className="text-sm">{texto}</p>
                 <div className="boton w-full
                 rounded-lg text-center p-5 " 
                 style={{
-                    background: `${props.fondo}`
+                    background: `${fondo}`
                 }}>
-                    <NavLink to={props.ruta} 
+                    <NavLink to={ruta} 
                     className="text-white 
                     font-semibold text-xl w-full">
-                        {props.etiqueta}
+                        {etiqueta}
                     </NavLink>
 
                 </div>
