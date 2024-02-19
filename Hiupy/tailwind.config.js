d45a17b9101fdf130a09fa2371ae62eb66411829
lucas id betan <lucas.idbetan@gmail.com>
2024-02-18 21:30:25 -0300

    t#2-login Agregué la paleta de colores a tailwind.config, avancé en el formulario de login

--- Hiupy/tailwind.config.js
+++ Hiupy/tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       
       fontFamily: {
         poppins: ['Poppins']  //instalacion de fuente poppins
+      },
+      colors: {
+        'amarillo-custom': '#FF9C00',
+        'botones': '#AF7D4D',
+        'light-grey': '#D9D9D9',
+        'dark-grey': '#A9A9A9',
+        'b-cocinar': '#C7786C',
+        'b-inventario': '#CACEB9',
+        'b-compras': '#E2A748'
       }
     },
   },
