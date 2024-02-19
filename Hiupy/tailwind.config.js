dc44ba3cd9e65249b8ca979f2bb9416d096ff74a
Pawli <paulitcodes@gmail.com>
2024-02-19 14:41:28 -0300

    anexo de paleta de colores

--- Hiupy/tailwind.config.js
+++ Hiupy/tailwind.config.js
@@ -4,15 +4,18 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        'af7d4d': '#AF7D4D',
+        af7d4d: "#AF7D4D",
       },
       textColor: {
-        'ff9c00': '#FF9C00',
+        ff9c00: "#FF9C00",
+        c7786c: "#C7786C",
+        caceb9: "#CACEB9",
+        e2a748: '#E2A748',
       },
       fontFamily: {
-        poppins: ['Poppins']  //instalacion de fuente poppins
-      }
+        poppins: ["Poppins"], //instalacion de fuente poppins
+      },
     },
   },
   plugins: [],
-}
+};
