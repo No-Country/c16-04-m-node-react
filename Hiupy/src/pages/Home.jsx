c5e02ae4872ece3313f5047197dac6e77e1d7076
Jean <jeanpolo15@gmail.com>
2024-02-20 11:16:04 -0500

    Reajuste de resolucion, cambios en la navbar

--- Hiupy/src/pages/Home.jsx
+++ Hiupy/src/pages/Home.jsx
@@ -4,21 +4,24 @@ import { Boton, NavBar, SearchBar } from "../components";
 export const Home = () => {
     /* const users = useSelector(selectAllUsers)
     console.log(users) */
+    const profileStyle = 'contain';
 
     return (
         <>
-            <div className="w-full font-poppins box-border flex flex-col h-screen justify-between">
-                <div className="px-6 flex flex-col h-full gap-y-14">
-                    <div className="flex w-full justify-between pt-4">
-                        <div className="flex-col">
+            <div className="w-full h-screen font-poppins box-border flex flex-col gap-y-4">
+                <div className="px-6 flex flex-col h-full gap-y-8">
+                    <div className="flex w-full pt-4">
+                        <div className="w-full">
                             <h2 className="font-semibold text-xl">Buenas noches usuario</h2>
                             <span className="text-dark-grey text-xs">¿Cómo vamos a alimentarnos?</span>
                         </div>
-                        <img src="/assets/images/Avatar.png" alt="Profile"/>
+                        <img src="/assets/images/Avatar.png" alt="Profile" style={{ 
+                            objectFit: profileStyle
+                        }}/>
                     </div>
 
-                    <SearchBar />
-                    <div className="space-y-8">
+                    <SearchBar placeholder="Inserta ingredientes con los que quieras cocinar" />
+                    <div className="space-y-6">
                         <Boton texto="Preparar o planificar nuestra comida" 
                         etiqueta="Cocinar" fondo="#C7786C" ruta="/" />
 
@@ -28,8 +31,6 @@ export const Home = () => {
                         <Boton texto="Conseguir los ingredientes que faltan" 
                         etiqueta="Lista de compras" fondo="#E2A748" ruta="" />
                     </div>
-
-                    <img src="/assets/images/comida.png" alt="comida" />
                 </div>
                 <NavBar />
             </div>
