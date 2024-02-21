4fb05eddfcec41e61654444042d6a9aec8b7dd0c
lucas id betan <lucas.idbetan@gmail.com>
2024-02-21 16:42:22 -0300

    Se va a eliminar searchBar e incorporar a la Navbar

--- Hiupy/src/components/SearchBar/SearchBar.jsx
+++ Hiupy/src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,10 @@
-export const SearchBar = () => {
+export const SearchBar = ({placeholder}) => {
     return (
         <div className=" flex w-full rounded-xl border border-light-grey justify-around">
             <div className=" self-center">
                 <img src="/assets/images/search.svg" alt="search-icon" />
             </div>
-            <input type="text" placeholder="Inserta ingredientes con los que quieras cocinar" className="placeholder:text-xs bg-transparent w-10/12 text-xs outline-none py-3" />
+            <input type="text" placeholder={placeholder} className="placeholder:text-xs bg-transparent w-10/12 text-xs outline-none py-3" />
         </div>
     )
 };
