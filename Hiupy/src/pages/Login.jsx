2e63e9ec7d42a55d8e0be8ee0690da109c6779be
lucas id betan <lucas.idbetan@gmail.com>
2024-02-22 13:31:14 -0300

    Testing pre presentacion 23/02

--- Hiupy/src/pages/Login.jsx
+++ Hiupy/src/pages/Login.jsx
@@ -2,13 +2,14 @@ import { Footer, LoginForm, NavBar } from "../components";
 
 export const Login = () => {
     return (
-        <>  <NavBar/>
+        <>
+            <NavBar />
             <div className="container p-8">
                 <h1 className="text-left text-xl">
                     <span className="font-semibold text-3xl">Hola,</span>
                     <br /> ingresa a tu cuenta
                 </h1>
-                <LoginForm/>
+                <LoginForm />
             </div>
             <Footer />
         </>
