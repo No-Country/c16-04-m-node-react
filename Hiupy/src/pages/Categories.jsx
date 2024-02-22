373edca22745f312dbc72c6d0a562b27d2f65248
Pawli <paulitcodes@gmail.com>
2024-02-22 13:23:52 -0300

    add footer t7-categories

--- Hiupy/src/pages/Categories.jsx
+++ Hiupy/src/pages/Categories.jsx
@@ -1,4 +1,4 @@
-import { NavBar } from "../components";
+import { NavBar, Footer } from "../components";
 import { useState } from "react";
 
 export const Categories = () => {
@@ -12,7 +12,6 @@ export const Categories = () => {
   return (
     <div className="flex flex-col m-1 text-center">
       <NavBar />
-
       <h3 className="m-2 text-left text-xl font-bold ">
         Agrega a tu inventario los ingredientes que tienes en tu casa{" "}
       </h3>
@@ -27,28 +26,29 @@ export const Categories = () => {
       <h3 className="text-center"> Comidas congeladas </h3>
       <img
         className=" w-4/3 h-3/6  rounded-2xl m-1"
-        src="/public/assets/images/alimentosCategories.png"
+        src="/assets/images/alimentosCategories.png"
       />
       <h3 className="text-center"> Alimentos frescos </h3>
       <img
-        className=" w-4/3 h-1/4 rounded-2xl m-1"
-        src="/public/assets/images/alimentosCategories.png"
+        className=" w-4/3 h-3/6 rounded-2xl m-1"
+        src="/assets/images/alimentosCategories.png"
       />
       <h3 className="text-center"> Alimentos secos </h3>
       <img
         className=" w-4/3 h-3/6  rounded-2xl m-1"
-        src="/public/assets/images/alimentosCategories.png"
+        src="/assets/images/alimentosCategories.png"
       />
       <h3 className="text-center"> Huerta </h3>
       <img
         className=" w-4/3 h-3/6 rounded-2xl m-1"
-        src="/public/assets/images/alimentosCategories.png"
+        src="/assets/images/alimentosCategories.png"
       />
       <div className="relative">
       <button className="fixed bottom-24 right-5 bg-e2a748 hover:bg-orange-900 text-white font-bold py-4 px-6 rounded-full">
         +
       </button>
       </div>
+      <Footer/>
     </div>
   );
 };
