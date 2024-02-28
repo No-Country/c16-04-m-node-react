8f93b67aa09a5a42524ba381dd14584743e5eea7
lucas id betan <lucas.idbetan@gmail.com>
2024-02-28 10:20:31 -0300

    Se unificaron estilos del componente Categories.jsx

--- Hiupy/src/pages/Categories.jsx
+++ Hiupy/src/pages/Categories.jsx
@@ -1,49 +1,47 @@
-import { NavBar, Footer } from "../components";
-import { useState } from "react";
+import { NavBar, Footer, SearchBar } from "../components";
+// import { useState } from "react";
 
 export const Categories = () => {
-    const [searchTerm, setSearchTerm] = useState("");
+    // const [searchTerm, setSearchTerm] = useState("");
 
-    const handleInputChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
+    // const handleInputChange = (event) => {
+    //     setSearchTerm(event.target.value);
+    // };
 
     return (
         <div>
-            <div className="flex flex-col m-1 text-center">
+            <div className="flex flex-col gap-3 m-1 text-center">
                 <NavBar />
 
                 <h3 className="m-2 text-left text-xl font-bold ">
                     Agrega a tu inventario los ingredientes que tienes en tu
                     casa{" "}
                 </h3>
-                <input
-                    className="m-2 border-2 h-10 border-gray-200 rounded-md"
-                    type="text"
-                    placeholder="Ingresa los ingredientes"
-                    value={searchTerm}
-                    onChange={handleInputChange}
+
+                <SearchBar
+                    className=""
+                    placeholder={"Ingresa los ingredientes"}
                 />
 
-                <h3 className="text-center"> Comidas congeladas </h3>
+                <h3 className="text-center text-sm"> Comidas congeladas </h3>
                 <img
-                    className=" w-4/3 h-3/6  rounded-2xl m-1"
-                    src="/public/assets/images/alimentosCategories.png"
+                    className=" w-4/3 h-[60px]  rounded-2xl m-1"
+                    src="/assets/images/congelados.png"
                 />
-                <h3 className="text-center"> Alimentos frescos </h3>
+                <h3 className="text-center text-sm"> Alimentos frescos </h3>
                 <img
-                    className=" w-4/3 h-1/4 rounded-2xl m-1"
-                    src="/public/assets/images/alimentosCategories.png"
+                    className=" w-4/3 h-[60px] rounded-2xl m-1"
+                    src="/assets/images/frescos.png"
                 />
-                <h3 className="text-center"> Alimentos secos </h3>
+                <h3 className="text-center text-sm"> Alimentos secos </h3>
                 <img
-                    className=" w-4/3 h-3/6  rounded-2xl m-1"
-                    src="/public/assets/images/alimentosCategories.png"
+                    className=" w-4/3 h-[60px]  rounded-2xl m-1"
+                    src="/assets/images/secos.png"
                 />
-                <h3 className="text-center"> Huerta </h3>
+                <h3 className="text-center text-sm"> Huerta </h3>
                 <img
-                    className=" w-4/3 h-3/6 rounded-2xl m-1"
-                    src="/public/assets/images/alimentosCategories.png"
+                    className=" w-4/3 h-[60px] rounded-2xl m-1"
+                    src="/assets/images/huerta.png"
                 />
                 <div className="relative">
                     <button className="fixed bottom-24 right-5 bg-e2a748 hover:bg-orange-900 text-white font-bold py-4 px-6 rounded-full">
