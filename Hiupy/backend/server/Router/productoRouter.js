8ee4c7815564410fe0be73bcccb4aa01ce7f6bab
Valentina <valenprofitos2001@gmail.com>
2024-02-28 21:17:16 -0300

    CRUD Terminado

--- Hiupy/backend/server/Router/productoRouter.js
+++ Hiupy/backend/server/Router/productoRouter.js
@@ -1,12 +1,17 @@
-import express from 'express';
-import { getProducts, createProduct, updateProduct, deleteProduct } from '../controller/productController.js';
+import express from "express";
+import {
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controller/productController.js";
 
 const router = express.Router();
 
 // Rutas para manejar las operaciones CRUD de productos
-router.get('/products', getProducts);
-router.post('/products', createProduct);
-router.put('/products/:id', updateProduct);
-router.delete('/products/:id', deleteProduct);
+router.get("/productos", getProducts);
+router.post("/productos", createProduct);
+router.put("/productos/:id_product", updateProduct);
+router.delete("/productos/:id_product", deleteProduct);
 
 export default router;
--- Hiupy/backend/server/controller/productController.js
+++ Hiupy/backend/server/controller/productController.js
@@ -1,30 +1,39 @@
-import Productos from '../models/Productos.js';
+import Products from "../models/Productos.js";
 
 // Controlador para manejar las operaciones CRUD de productos
 
 // Obtener todos los productos
 async function getProducts(req, res) {
-    try {
-        const products = await Productos.findAll();
-        res.json(products);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const products = await Products.findAll();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 //Crear un nuevo producto
 async function createProduct(req, res) {
-    if (!req.body) {
-        console.log(req.body)
-        return res.status(400).json({ message: 'No se proporcionaron datos en el cuerpo de la solicitud.' });
-    }
-    try {
-        const { Nombre, Categoria } = req.body;
-        const newProduct = await Productos.create({ Nombre, Categoria });
-        res.status(201).json(newProduct);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  if (!req.body) {
+    console.log(req.body);
+    return res
+      .status(400)
+      .json({
+        message: "No se proporcionaron datos en el cuerpo de la solicitud.",
+      });
+  }
+  try {
+    const { product_name, description, category, location } = req.body;
+    const newProduct = await Products.create({
+      product_name,
+      description,
+      category,
+      location,
+    });
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
 //async function createProduct(req, res) {
@@ -36,33 +45,30 @@ async function createProduct(req, res) {
 //    }
 //}
 
-
-
-
 // Actualizar un producto existente
 async function updateProduct(req, res) {
-    const { id } = req.params;
-    try {
-        await Productos.update(req.body, {
-            where: { id }
-        });
-        res.status(200).json({ message: 'Producto actualizado correctamente.' });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  const { id_product } = req.params;
+  try {
+    await Products.update(req.body, {
+      where: { id_product },
+    });
+    res.status(200).json({ message: "Producto actualizado correctamente." });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
 // Eliminar un producto
 async function deleteProduct(req, res) {
-    const { id } = req.params;
-    try {
-        await Productos.destroy({
-            where: { id }
-        });
-        res.status(200).json({ message: 'Producto eliminado correctamente.' });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  const { id_product } = req.params;
+  try {
+    await Products.destroy({
+      where: { id_product },
+    });
+    res.status(200).json({ message: "Producto eliminado correctamente." });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
 export { getProducts, createProduct, updateProduct, deleteProduct };
