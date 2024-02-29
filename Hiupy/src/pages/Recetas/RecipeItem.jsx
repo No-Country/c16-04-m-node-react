dbde7cf21c70684bc1d16183b1f548a179eb419c
lucas id betan <lucas.idbetan@gmail.com>
2024-02-29 13:19:04 -0300

    Se agregaron las rutas para las imágenes en las recetas

--- Hiupy/src/pages/Recetas/RecipeItem.jsx
+++ Hiupy/src/pages/Recetas/RecipeItem.jsx
@@ -3,7 +3,7 @@ export const RecipeItem = ({ receta }) => {
         <>
             <img
                 className="w-[150px] h-[150px] rounded-2xl"
-                src={receta.img}
+                src={receta.img_url}
             />
             <h2 className="font-semibold text-sm">{receta.recipe_name}</h2>
             {/* <h2 className="font-semibold text-sm">{receta.description}</h2> */}
