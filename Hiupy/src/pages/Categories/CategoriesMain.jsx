52b40f145958775d2c5d254227eabdc5405ba3f9
lucas id betan <lucas.idbetan@gmail.com>
2024-03-01 12:26:44 -0300

    probando

--- Hiupy/src/pages/Categories/CategoriesMain.jsx
+++ Hiupy/src/pages/Categories/CategoriesMain.jsx
@@ -12,23 +12,23 @@ export const CategoriesMain = () => {
     return (
         <div>
             <NavBar />
-            <div className="flex flex-col gap-3 items-center mx-auto w-11/12 text-center">
+            <div className="flex flex-col gap-8 items-center mx-auto w-11/12 text-center">
                 <h3 className="m-2 text-left text-xl font-bold ">
                     Agrega a tu inventario los ingredientes que tienes en tu
                     casa
                 </h3>
 
-                <SearchBar
+                {/* <SearchBar
                     className=""
                     placeholder={"Ingresa los ingredientes"}
-                />
+                /> */}
                 <NavLink to="/Congelados">
                     <h3 className="text-center text-sm">Comidas congeladas</h3>
                     <img
                         className=" w-4/3 h-[60px]  rounded-2xl m-1"
                         src="/assets/images/congelados.png"
                     />
-                    <p className="text-xs text-dark-grey">¿Que vas a guardar en el freezer?</p>
+                    <p className="text-[10px] text-dark-grey">¿Que vas a guardar en el freezer?</p>
                 </NavLink>
 
                 <NavLink to="/Frescos">
@@ -37,7 +37,7 @@ export const CategoriesMain = () => {
                         className=" w-4/3 h-[60px] rounded-2xl m-1"
                         src="/assets/images/frescos.png"
                     />
-                    <p className="text-xs text-dark-grey">¿Que vas a guardar en la heladera?</p>
+                    <p className="text-[10px] text-dark-grey">¿Que vas a guardar en la heladera?</p>
                 </NavLink>
 
                 <NavLink to="/Secos">
@@ -46,7 +46,7 @@ export const CategoriesMain = () => {
                         className=" w-4/3 h-[60px]  rounded-2xl m-1"
                         src="/assets/images/secos.png"
                     />
-                    <p className="text-xs text-dark-grey">¿Que vas a guardar en la alacena?</p>
+                    <p className="text-[10px] text-dark-grey">¿Que vas a guardar en la alacena?</p>
                 </NavLink>
 
                 <NavLink to="/Huerta">
@@ -55,7 +55,7 @@ export const CategoriesMain = () => {
                         className=" w-4/3 h-[60px] rounded-2xl m-1"
                         src="/assets/images/huerta.png"
                     />
-                    <p className="text-xs text-dark-grey">¿Que tienes en en la huerta?</p>
+                    <p className="text-[10px] text-dark-grey">¿Que tienes en la huerta?</p>
                 </NavLink>
 
                 <div className="relative">
