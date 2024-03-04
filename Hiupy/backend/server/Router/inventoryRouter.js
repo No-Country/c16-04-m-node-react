2c22f090c05cd33428eb9ac37e665e80cfd90c17
Facurro <facundo.chiabotto@gmail.com>
2024-03-04 16:21:13 -0300

    correcion ruta inv

---
+++ Hiupy/backend/server/Router/inventoryRouter.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { getProductInventory } from "../controller/productInventoryController.js";
+const inventoryRouter = express.Router()
+
+
+inventoryRouter.get('/inventory/:id', getProductInventory)
