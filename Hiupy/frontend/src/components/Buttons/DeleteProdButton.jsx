33d40f16471b15b0b9aee41af2aef958bef1b0ee
lucas id betan <lucas.idbetan@gmail.com>
2024-03-07 17:38:53 -0300

    Se agregó botón de eliminar producto (todavía no funciona como tal)

--- Hiupy/frontend/src/components/Buttons/DeleteProdButton.jsx
+++ Hiupy/frontend/src/components/Buttons/DeleteProdButton.jsx
@@ -2,7 +2,7 @@ export const DeleteProdButton = ({onClick}) => {
   return (
     <div>
       <button onClick={onClick}>
-        <img src="/assets/images/delete.png" className="w-[18px] h-[20px]"/>  
+        <img src="/assets/images/delete.png" className="w-[14px] h-[16px]"/>  
       </button>
       
     </div>
